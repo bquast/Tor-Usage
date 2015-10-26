89aa9b33ffd05baf55d8f2f2739b3f1f94ec943a
Bastiaan Quast <bquast@gmail.com>
2015-10-26 14:41:35 +0000

    work map

--- import.R
+++ import.R
@@ -2,11 +2,14 @@
 library(rgdal)
 library(dplyr)
 library(ggplot2)
+library(leaflet)
 
 # import the Data
 url <- "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip"
 file <- basename(url) 
 download.file(url, file)
+folder <- getwd()
+unzip(file, exdir = folder)
 
 # read the data
 clients <- read.csv("clients.csv")
@@ -20,17 +23,31 @@ world <- readOGR(dsn = folder,
 str(clients)
 head(clients)
 
-clients$region <- clients$country %>%
-  toupper() %>%
-  countrycode("iso2c", "country.name") %>%
-  tolower()
-
-head(clients)
-
 # create some basic stats
 avg <- clients %>%
-  group_by(region) %>%
+  group_by(country) %>%
   summarise(value=mean(clients, na.rm=TRUE))
 
-country_choropleth(na.omit(avg))
+avg$country %<>%
+  toupper()
+
+avg$country %<>%
+  countrycode("iso2c", "iso3c")
+
+avg %<>%
+  na.omit()
+
+avg2 <- merge(avg, world, by.x="country", by.y="adm0_a3")
+
+new   <- merge(x = world,
+               y = avg2,
+               by.x = "adm0_a3",
+               by.y = "country",
+               all.x = TRUE)
+
+pal <- colorQuantile("YlOrRd", NULL, n = 20)
+
+leaflet(data = new) %>%
+  addTiles() %>%
+  addPolygons(fillColor = ~pal(world$pop_est))
 
